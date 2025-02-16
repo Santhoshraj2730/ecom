4964e630115833444d709579fb1e291ab73c6203
Santhosh Raj V <140722890+Santhoshraj2730@users.noreply.github.com>
2025-02-16 16:32:48 +0530

    Update app.py
    sdfgh

--- Python-based/app.py
+++ Python-based/app.py
@@ -246,4 +246,5 @@ def category_bag():
     return render_template('category_bag.html', items=items)
 # Main
 if __name__ == '__main__':
+    port=int(os.environ.get("PORT",4000))
     app.run(host="0.0.0.0",port=port)
